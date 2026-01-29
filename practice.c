7f3ae0e020c51e6f0d323de7f787de52c4bb10be
ramprasad51 <ramprasad2007my@gmail.com>
2026-01-29 19:17:43 +0530

    Programm displays GRADE of a student using else-if ladder

--- practice.c
+++ practice.c
@@ -1,85 +1,33 @@
-// KYC verification using arrays and switch-case
-#include<stdio.h>
-
-int main()
+// C program that takes a student marks as input and prints the corresponding grade
+#include <stdio.h>
+void main()
 {
-    int pan[]={1111,2222,3333};
-    int aadhar[]={4444,5555,6666};  
-    int aapar[]={7777,8888,9999};
-    int DL[]={9999,1234};
-    int passport[]={4321,8765};
-
-    int choice,id,i,found=0;
-
-    printf("KYC verification system\n");
-    printf("1. PAN number\n");
-    printf("2. Aadhar number\n");
-    printf("3. APAR number\n");
-    printf("4. Driving License\n");
-    printf("5. Passport number\n");
-
-    printf("Enter your choice: \n");
-    scanf("%d",&choice);
-
-    printf("Enter your ID number: \n");
-    scanf("%d",&id);
-
-    switch (choice)
+    int marks;
+    printf("Enter the student marks: ");
+    scanf("%d",&marks);
+    
+    if(marks<0 || marks>100)
     {
-        case 1:
-            for(i=0;i<3;i++)
-                if(pan[i]==id)
-                { 
-                    found=1;
-                     break; 
-                    }
-            break;
-
-        case 2:
-            for(i=0;i<3;i++)
-                if(aadhar[i]==id)
-                {
-                     found=1;
-                     break; 
-                    }
-            break;
-
-        case 3:
-            for(i=0;i<3;i++)
-                if(aapar[i]==id)
-                {
-                     found=1;
-                     break;
-                     }
-            break;
-
-        case 4:
-            for(i=0;i<2;i++)
-                if(DL[i]==id)
-                {
-                     found=1; 
-                    break; 
-                }
-            break;
-
-        case 5:
-            for(i=0;i<2;i++)
-                if(passport[i]==id)
-                {
-                     found=1; 
-                    break; 
-                }
-            break;
-
-        default:
-            printf("Invalid choice\n");
-            return 0;
+        printf("Invalid marks\n");
     }
-
-    if(found)
-        printf(" Verified : ID found in the records\n");
+    else if(marks>=90)
+    {
+        printf("Grade: A\n");
+    }
+    else if(marks>=75)
+    {
+        printf("Grade: B\n");
+    }
+    else if(marks>=60)
+    {
+        printf("Grade: C\n");
+    }
+    else if(marks>=50)
+    {
+        printf("Grade: D\n");
+    }
     else
-        printf(" Not Verified : ID not found in the records\n");
-
-    return 0;
+    {
+        printf("Grade: F\n");
+    }
 }
