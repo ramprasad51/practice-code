062fc0983a8fee2ace4dcb0f675de11bf56b5894
ramprasad51 <ramprasad2007my@gmail.com>
2026-01-29 19:51:49 +0530

    Implement function to swap two account balances

--- practice.c
+++ practice.c
@@ -1,22 +1,21 @@
-// C program to calculate the straight line distance between two points 
-#include<stdio.h>
-#include<math.h>
-void main() 
+// C program that swap two accounts' balances using functions(swap by reference)
+#include <stdio.h>
+void swap(int *a,int *b)
 {
-    float x1, y1, x2, y2;
-    float distance;
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
 
-
-    // Input coordinates of the first point
-    printf("Enter the first coordinates: ");
-    scanf("%f %f", &x1, &y1);
-    // Input coordinates of the second point
-    printf("Enter the second coordinates: ");
-    scanf("%f %f", &x2, &y2);
-
-    // Calculate the distance
-    distance=sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-
-    printf("The straight line distance between the two points is: %.2f\n", distance);
-
-}    
+void main()
+{
+    int balance1,balance2;
+    printf("Enter balance of account 1: ");
+    scanf("%d", &balance1);
+    printf("Enter balance of account 2: ");
+    scanf("%d", &balance2);
+    printf("account balance Before swapping: Account 1 = %d\n Account 2 = %d\n", balance1, balance2);
+    swap(&balance1, &balance2);
+    printf("account balance After swapping: Account 1 = %d\n Account 2 = %d\n", balance1, balance2);
+}
