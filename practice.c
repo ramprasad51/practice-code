909a4eecb3d407ba4e639af0d53b4732fd1ff1ae
ramprasad51 <ramprasad2007my@gmail.com>
2026-01-29 11:26:35 +0530

    Implement keyword search in course description and calculate its length

--- practice.c
+++ practice.c
@@ -1,24 +1,29 @@
-#include<stdio.h>
-#include<math.h>
-#define PI 3.14
-void main()
+//To check a keyword(one string) in given course description(another string) and also find the length of the course description.using string functions.
+#include <stdio.h>
+#include<string.h>
+#define MAX 100
+int main()
 {
-    float sum,term,x,num;
-    int degree;
-    int i=2,fact=1;
-    printf("Enter degree: ");
-    scanf("%d",&degree);
-    x=degree*PI/180;//conversion of degree to radian(x)
-    sum = x;
-    term=x;
-    num=x*x; //stores x^2 to avoid recalculation
-    while(fabs(term)>=0.0001) //fabs is used to get absolute value of float
-    {
-        fact=fact*i*i+1;//factorial calculation
-        term=term*(-num)/fact;//calculation of each term
-        sum+=term;//stores the final result of sin(x)
-        i=i+2;//skips even power
+    char description[MAX];
+    char keyword[MAX];
+    int len;
+    printf("Enter course description: ");
+    gets(description);
+    printf("Enter keyword to search: ");
+    gets(keyword);
+    len=strlen(description);
+    printf("The length of course description is:%d\n",len);
 
-    }
-    printf("Approx sin(%d)=%.2f\n",degree,sum);//prints the result
+    if(strstr(description , keyword) != NULL)
+
+{
+    printf("Keyword \"%s\" found in \"%s\" description.\n", keyword, description);
+
+}
+
+else
+{
+    printf("Keyword \"%s\" not found in \"%s\" description.\n", keyword, description);
+}
+    return 0;
 }
