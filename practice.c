b056b4a93adaecc7c774268d6deb3515cb010145
ramprasad51 <ramprasad2007my@gmail.com>
2026-01-31 21:11:36 +0530

    program to read integers 'n' and calculate their sum

--- practice.c
+++ practice.c
@@ -1,52 +1,22 @@
-//Finding a B00k ID with help of Binary search
+//C programm that  reads n integers,stores them in an array, and calculates their sum
+
 #include<stdio.h>
 int main()
 {
-    int bookid[35];
-    int low,mid,high,key,loc=-1;
-    int i,n;
+    int i,n,sum=0;
+    int arr[100];
 
-    printf("Enter the num of books: ");
+    printf("Enter the number of integers:  ");
     scanf("%d",&n);
 
-    printf("Enter the the %dbooks in ascending order:  \n",n);
+    printf("Enter %d integers: \n",n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&bookid[i]);
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
     }
 
-    printf("Enter the bookid to search: ");
-    scanf("%d",&key);
-    low=0;
-    high=n-1;
-    while(low<=high)
-    {
-        mid=(low+high)/2;
-    
-
-    if(bookid[mid]==key)
-    {
-        loc=mid;
-        break;
-        
-    }
-    else if(bookid[mid]<key)
-    {
-        low=mid+1;
-    }
-    else
-    {
-        high=mid-1;
-    }
-    }
-
-    if(loc!=-1)
-    {
-        printf("Element %d found at location %d\n",key,loc+1);
-    }
-    else
-    {
-       printf("Element not found \n");
-    }
+    printf("Sum of the integers is: %d\n",sum);
     return 0;
+
 }
