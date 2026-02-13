c1b9c0c3aedc8c20e3bb0c72a096cac0e7e27518
ramprasad51 <ramprasad2007my@gmail.com>
2026-02-13 22:32:02 +0530

    Implement program to read integers  & inputting them into array and calculate maximum, minimum, and average using functions&pointers

--- practice.c
+++ practice.c
@@ -1,9 +1,40 @@
-//To find greatest of two numbers using conditional operator
+//C programm to read n integers into an array and finds the maximum,minimum,and average of elements of array using a function & p*inters
 #include<stdio.h>
-void main()
+//Function prototype
+void analyze(int *a,int n,int *max,int *min,float *avg);
+int main()
 {
-    int a,b;
-    printf("Enter any two number: ");
-    scanf("%d %d",&a,&b);
-    printf(a>=b?"a is larger": "b is larger");//conditional operator  (? :)
+    int a[100],n,i;
+    int max,min;
+    float avg;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+
+    printf("Enter elements: \n");
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+
+    analyze(a,n,&max,&min,&avg);//Function call
+
+    printf("Largest=%d\n",max);
+    printf("Smallest=%d\n",min);
+    printf("Average=%.2f\n",avg);
+    return 0;
+}
+void analyze(int *a,int n,int *max,int *min,float *avg)//Function definition
+{
+    int i,sum=0;
+    *max=*min=a[0];//Initialize max and min with the first element of the array
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>*max)
+        *max=a[i];
+
+        if(a[i]<*min)
+        *min=a[i];
+
+        sum+=a[i];
+    }
+    *avg=(float)sum/n;
+
 }
