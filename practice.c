08f52d33ccaaacd55662f0eaa5a25fe75ac0a5d7
ramprasad51 <ramprasad2007my@gmail.com>
2026-02-14 16:09:24 +0530

    passing entire structure to function using call by value

--- practice.c
+++ practice.c
@@ -1,27 +1,27 @@
-//Arrays within structures
+//Passing entire structure to a function using call by value
 #include <stdio.h>
 struct student
-{
-    int usn;
-    int marks[3];//array inside structure
+{ 
+    int roll;
+    float marks;
 };
+//Function prototype
+void display(struct student s);
 
-int main()
+void main()
 {
     struct student s;
-    int i;
-    s.usn=93;
-
-    printf("Enter  3 subject marks:\n");//taking input for array inside structure
-    for(i=0;i<3;i++)
-    {
-        scanf("%d",&s.marks[i]);
-    }
-    printf("USN:%d\n Marks:\n",s.usn);//printing the array elements of structure
-    for(i=0;i<3;i++)
-    {
-        printf("%d\t",s.marks[i]);
-    }
-    return 0;
-
+    printf("Enter roll number: ");
+    scanf("%d",&s.roll);
+    printf("Enter marks: ");
+    scanf("%f",&s.marks);
+//Function call
+    display(s);
+}
+//Function definition
+void display(struct student s)
+{
+    printf("\n Student deatils:\n");
+    printf("Roll:%d\n",s.roll);
+    printf("Marks:%.2f\n",s.marks);
 }
