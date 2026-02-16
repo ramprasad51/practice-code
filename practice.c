ce4c26638aa1d930de4796c8e62513308e51340c
ramprasad51 <ramprasad2007my@gmail.com>
2026-02-16 10:40:36 +0530

    program to perform multiplication, subtraction, and division on two whole numbers

--- practice.c
+++ practice.c
@@ -1,12 +1,26 @@
-//C program to calculate simple interest
+// This program performs multiplication, subtraction, and division on two whole numbers entered by the user.
 #include<stdio.h>
 void main()
 {
-    float p,t,r,si;
-    printf("Enter principal(p),time(t),rate(r): ");
-    scanf("%f %f %f",&p,&t,&r);
-//formula for simple interest
-    si=(p*t*r)/100;
+    int a,b;
+    int mul,sub;
+    float div;
 
-    printf("Simple interest=%.2f",si);
+    printf("Enter two whole numbers: ");
+    scanf("%d %d",&a,&b);
+
+    mul=a*b;
+    sub=a-b;
+    printf("Multiplication result= %d\n",mul);
+    printf("Subtraction result=%d\n",sub);
+
+    if(b!=0)
+    {
+        div=(float)a/b;
+        printf("Division result=%.2f\n",div);
+    }
+    else
+    {
+        printf("Division by zer0 is n0t all0wed");
+    }
 }
