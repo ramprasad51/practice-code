4a0790d372cfced3f150fd92fc45b0c90839d152
ramprasad51 <ramprasad2007my@gmail.com>
2026-02-16 07:21:51 +0530

    program to demonstrate variable length arrays (VLA)

--- practice.c
+++ practice.c
@@ -1,16 +1,19 @@
-//Accessing array elements using pointer
+//Programm to understand variable length array(VLA) 
 #include<stdio.h>
 int main()
 {
-    int a[5]={12,24,36,48,60};
-    int i;
-    int *p;
-    p=a;//or p=&a[0];//pointer point to base address of array
+    int i,n;
+    printf("Enter array size: ");
+    scanf("%d",&n);
+    int a[n];//VLA declaration
 
-    printf("Array elemets using pointer:\n");
-    for(i=0;i<5;i++)
+    for(i=0;i<n;i++)
+    a[i]=i+1;//incrementing array elements from 1 to n
+
+    printf("Array elements:\n");
+    for(i=0;i<n;i++)
     {
-        printf("%d\t",*(p+i));//p+i gives the address of the ith element and * dereferences it to get the value
+        printf("%d\t",a[i]);
     }
     return 0;
 }
