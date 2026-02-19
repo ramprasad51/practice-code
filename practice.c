645f709b544ab54a495e89fb2b9a8fd780c9e186
ramprasad51 <ramprasad2007my@gmail.com>
2026-02-19 22:08:22 +0530

    program to store and display employee details using a structure

--- practice.c
+++ practice.c
@@ -1,33 +1,32 @@
-//Program in which we will pass an array of structure to a function and display the details of students.
+//Store and display employee details using structure
 #include<stdio.h>
-//Defining a structure named student
-struct student
+//structure declaration
+struct employee
 {
-    int roll;
-    char name[25];
-    float marks;
+    int emp_id;
+    char name[20];
+    float salary;
+   long long int ph_num;//to store 10 digit phone number we can use long long int data type
 };
-//Function to display the details of students
-void display(struct student s[],int n)
-{
-    int i;
-    for(i=0;i<n;i++)//Loop to display the details of students
-    {
-        printf("\n Roll no:%d",s[i].roll);
-        printf("\n Name:%s",s[i].name);
-        printf("\n Marks:%.2f",s[i].marks);
-    }
-}
 
 int main()
-{//Declaring an array of structure and initializing it with the details of students
-    struct student s[2]={
-        {1,"Ram",87.5},
-        {3,"Prasad",93}
-    };
-    //Calling the display function to display the details of student
-    display(s,2);
+{
+    struct employee e;//structure variable declaration
+//Reading employee details from user
+    printf("Enter Employee id: ");
+    scanf("%d",&e.emp_id);
+    printf("Enter employee name: ");
+    scanf("%s",e.name);
+    printf("Enter Salary: ");
+    scanf("%f",&e.salary);
+    printf("Enter Phone number: ");
+    scanf("%lld",&e.ph_num);
+//Displaying employee details
+    printf("\n~~~ Employee deatils ~~~\n");
+    printf("Employee ID:%d\n",e.emp_id);
+    printf("Employee Name:%s\n",e.name);
+    printf("Employee salary:%.2f\n",e.salary);
+    printf("Employee phone number:%lld",e.ph_num);
 
     return 0;
 }
-
